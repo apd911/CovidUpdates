370dc098ac8b9541b7a2d67aea46053ba4068328
Massimiliano Figliola <apd291@gmail.com>
2021-04-12 17:00:10 +0200

    Updated base code, added graphs

---
+++ CovidItalia/MainWindow.xaml.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.ComponentModel;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LibCovid;
+using LiveCharts;
+using LiveCharts.Wpf;
+using LiveCharts.Charts;
+
+namespace CovidItalia
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            string format = "#,##0";
+
+            InitializeComponent();
+
+            var values = Parser.Filtered(DateTime.Today);
+            var value = values[0];
+
+            regioni.Text = "-- Seleziona --";
+
+            labels.Text = "Nuovi Contagi\n" + "Totale Positivi\n" + "Nuovi Deceduti\n" + "Totale Deceduti\n" + "Nuovi Tamponi\n" + "Totale Tamponi\n" + "Totale Guariti\n" + "Totale Casi";
+            labelsr.Text = "Nuovi Contagi\n" + "Totale Positivi\n" + "Nuovi Deceduti\n" + "Totale Deceduti\n" + "Nuovi Tamponi\n" + "Totale Tamponi\n" + "Totale Guariti\n" + "Totale Casi\n" + "Regione";
+
+            data.Text = value.data.ToString("dd MMMM yyyy");
+            datar.Text = value.data.ToString("dd MMMM yyyy");
+            dati.Text = value.nuoviPositivi.ToString(format) + "\n" + value.totalePositivi.ToString(format) + "\n" + value.nuoviDeceduti.ToString(format) + "\n" +
+                value.totaleDeceduti.ToString(format) + "\n" + value.nuoviTamponi.ToString(format) + "\n" + value.totaleTamponi.ToString(format) + "\n" +
+                value.totaleGuariti.ToString(format) + "\n" + value.totaleCasi.ToString(format);
+            datir.Text = value.nuoviPositivi.ToString(format) + "\n" + value.totalePositivi.ToString(format) + "\n" + value.nuoviDeceduti.ToString(format) + "\n" +
+                value.totaleDeceduti.ToString(format) + "\n" + value.nuoviTamponi.ToString(format) + "\n" + value.totaleTamponi.ToString(format) + "\n" +
+                value.totaleGuariti.ToString(format) + "\n" + value.totaleCasi.ToString(format) + "\nItalia";
+            SeriesCollection = new SeriesCollection{};
+            DataContext = this;
+        }
+        private void DateChanged(object sender, SelectionChangedEventArgs e)
+
+        {
+            string format = "#,##0";
+            DateTime selected = (DateTime)calendar.SelectedDate;
+            var values = Parser.Filtered(selected);
+            var value = values[0];
+
+            data.Text = value.data.ToString("dd MMMM yyyy");
+            dati.Text = value.nuoviPositivi.ToString(format) + "\n" + value.totalePositivi.ToString(format) + "\n" + value.nuoviDeceduti.ToString(format) + "\n" +
+                value.totaleDeceduti.ToString(format) + "\n" + value.nuoviTamponi.ToString(format) + "\n" + value.totaleTamponi.ToString(format) + "\n" +
+                value.totaleGuariti.ToString(format) + "\n" + value.totaleCasi.ToString(format);
+        }
+
+        private void SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string format = "#,##0";
+            ComboBoxItem cbi = (ComboBoxItem)regioni.SelectedItem;
+            DateTime selected = (DateTime)calendarr.SelectedDate;
+            string regione = "Abruzzo";
+            if (cbi != null) { regione = cbi.Content.ToString(); }
+
+            var valuesr = Parser.FilteredRegioni(selected, regione);
+            var valuer = valuesr[0];
+
+            datar.Text = valuer.data.ToString("dd MMMM yyyy");
+            datir.Text = valuer.nuoviPositivi.ToString(format) + "\n" + valuer.totalePositivi.ToString(format) + "\n" + valuer.nuoviDeceduti.ToString(format) + "\n" +
+               valuer.totaleDeceduti.ToString(format) + "\n" + valuer.nuoviTamponi.ToString(format) + "\n" + valuer.totaleTamponi.ToString(format) + "\n" +
+               valuer.totaleGuariti.ToString(format) + "\n" + valuer.totaleCasi.ToString(format) + "\n" + valuer.regione;
+        }
+
+        public SeriesCollection SeriesCollection { get; set; }
+        public Func<double, string> XFormatter { get; set; }
+
+        private void start_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            end.DisplayDateStart = start.SelectedDate.Value.AddDays(1);
+            end.IsEnabled = true;
+        }
+
+        private void end_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            checkboxes.IsEnabled = true;
+        }
+
+        private void reset_Click(object sender, RoutedEventArgs e)
+        {
+            SeriesCollection.Clear();
+            if (nuoviPositivi.IsChecked == true)
+            {
+                SeriesCollection.Add(
+                new LineSeries
+                {
+                    Title = "Nuovi Contagi",
+                    Values = Parser.ChartSelector.nuoviPositivi(start.SelectedDate.Value, end.SelectedDate.Value),
+                    LineSmoothness = 0,
+                    PointGeometry = null,
+                    StrokeThickness = 1,
+                    Fill = Brushes.Transparent
+                });
+            }
+
+            if (totalePositivi.IsChecked == true)
+            {
+                SeriesCollection.Add(
+                new LineSeries
+                {
+                    Title = "Totale Positivi",
+                    Values = Parser.ChartSelector.totalePositivi(start.SelectedDate.Value, end.SelectedDate.Value),
+                    LineSmoothness = 0,
+                    PointGeometry = null,
+                    StrokeThickness = 1,
+                    Fill = Brushes.Transparent
+                });
+            }
+
+            if (nuoviDeceduti.IsChecked == true)
+            {
+                SeriesCollection.Add(
+                new LineSeries
+                {
+                    Title = "Nuovi Deceduti",
+                    Values = Parser.ChartSelector.nuoviDeceduti(start.SelectedDate.Value, end.SelectedDate.Value),
+                    LineSmoothness = 0,
+                    PointGeometry = null,
+                    StrokeThickness = 1,
+                    Fill = Brushes.Transparent
+                });
+            }
+
+            if (totaleDeceduti.IsChecked == true)
+            {
+                SeriesCollection.Add(
+                new LineSeries
+                {
+                    Title = "Totale Deceduti",
+                    Values = Parser.ChartSelector.totaleDeceduti(start.SelectedDate.Value, end.SelectedDate.Value),
+                    LineSmoothness = 0,
+                    PointGeometry = null,
+                    StrokeThickness = 1,
+                    Fill = Brushes.Transparent
+                });
+            }
+
+            if (nuoviTamponi.IsChecked == true)
+            {
+                SeriesCollection.Add(
+                new LineSeries
+                {
+                    Title = "Nuovi Tamponi",
+                    Values = Parser.ChartSelector.nuoviTamponi(start.SelectedDate.Value, end.SelectedDate.Value),
+                    LineSmoothness = 0,
+                    PointGeometry = null,
+                    StrokeThickness = 1,
+                    Fill = Brushes.Transparent
+                });
+            }
+
+            if (totaleTamponi.IsChecked == true)
+            {
+                SeriesCollection.Add(
+                new LineSeries
+                {
+                    Title = "Totale Tamponi",
+                    Values = Parser.ChartSelector.totaleTamponi(start.SelectedDate.Value, end.SelectedDate.Value),
+                    LineSmoothness = 0,
+                    PointGeometry = null,
+                    StrokeThickness = 1,
+                    Fill = Brushes.Transparent
+                });
+            }
+
+            if (totaleGuariti.IsChecked == true)
+            {
+                SeriesCollection.Add(
+                new LineSeries
+                {
+                    Title = "Totale Guariti",
+                    Values = Parser.ChartSelector.totaleGuariti(start.SelectedDate.Value, end.SelectedDate.Value),
+                    LineSmoothness = 0,
+                    PointGeometry = null,
+                    StrokeThickness = 1,
+                    Fill = Brushes.Transparent
+                });
+            }
+
+            if (totaleCasi.IsChecked == true)
+            {
+                SeriesCollection.Add(
+                new LineSeries
+                {
+                    Title = "Totale Casi",
+                    Values = Parser.ChartSelector.totaleCasi(start.SelectedDate.Value, end.SelectedDate.Value),
+                    LineSmoothness = 0,
+                    PointGeometry = null,
+                    StrokeThickness = 1,
+                    Fill = Brushes.Transparent
+                });
+                
+            }
+            xaxis.Labels = Parser.ChartSelector.data(start.SelectedDate.Value, end.SelectedDate.Value);
+        }
+    }
+}
---
+++ LibCovid/Class1.cs
@@ -0,0 +1,375 @@
+﻿using CsvHelper;
+using LazyCache;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Net;
+using LiveCharts;
+using LiveCharts.Wpf;
+using LiveCharts.Configurations;
+
+namespace LibCovid
+{
+    public class DateModel
+    {
+        public System.DateTime DateTime { get; set; }
+        public double Value { get; set; }
+    }
+
+    public static class Parser
+    {
+        public class Dati
+        {
+            public DateTime data { get; set; }
+            public string regione { get; set; }
+            public int nuoviPositivi { get; set; }
+            public int totaleCasi { get; set; }
+            public int totaleDeceduti { get; set; }
+            public int totaleTamponi { get; set; }
+            public int totaleGuariti { get; set; }
+            public int totalePositivi { get; set; }
+            public int nuoviDeceduti { get; set; }
+            public int nuoviTamponi { get; set; }
+        }
+
+        public static List<Dati> ReadCSV()
+        {
+            string source = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv";
+            StringReader rawFile = new StringReader(new WebClient().DownloadString(source));
+
+            using (var reader = rawFile) using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                List<Dati> records = new List<Dati>();
+                csv.Read();
+                csv.ReadHeader();
+                while (csv.Read())
+                {
+                    Dati regionep = new Dati();
+                    regionep.totaleTamponi = 0;
+                    regionep.totaleDeceduti = 0;
+                    if (records.Count > 1)
+                    {
+                        regionep.totaleTamponi = records[records.Count - 1].totaleTamponi;
+                        regionep.totaleDeceduti = records[records.Count - 1].totaleDeceduti;
+                    }
+                    var record = new Dati
+                    {
+                        data = csv.GetField<DateTime>("data"),
+                        regione = "italia",
+                        nuoviPositivi = csv.GetField<int>("nuovi_positivi"),
+                        totaleCasi = csv.GetField<int>("totale_casi"),
+                        totaleDeceduti = csv.GetField<int>("deceduti"),
+                        totaleTamponi = csv.GetField<int>("tamponi"),
+                        totaleGuariti = csv.GetField<int>("dimessi_guariti"),
+                        totalePositivi = csv.GetField<int>("totale_positivi"),
+                        nuoviDeceduti = (csv.GetField<int>("deceduti") - regionep.totaleDeceduti),
+                        nuoviTamponi = (csv.GetField<int>("tamponi") - regionep.totaleTamponi)
+                    };
+
+                    records.Add(record);
+                }
+
+                return records;
+            }
+        }
+
+        public static List<Dati> ReadCSVRegioni()
+        {
+            string source = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni.csv";
+            StringReader rawFile = new StringReader(new WebClient().DownloadString(source));
+
+            using (var reader = rawFile) using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                List<Dati> records = new List<Dati>();
+                csv.Read();
+                csv.ReadHeader();
+                while (csv.Read())
+                {
+                    Dati regionep = new Dati();
+                    regionep.totaleTamponi = 0;
+                    regionep.totaleDeceduti = 0;
+                    if (records.Count > 21)
+                    {
+                        regionep.totaleTamponi = records[records.Count - 21].totaleTamponi;
+                        regionep.totaleDeceduti = records[records.Count - 21].totaleDeceduti;
+                    }
+
+                    var record = new Dati
+                    {
+                        data = csv.GetField<DateTime>("data"),
+                        regione = csv.GetField("denominazione_regione").ToString(),
+                        nuoviPositivi = csv.GetField<int>("nuovi_positivi"),
+                        totaleCasi = csv.GetField<int>("totale_casi"),
+                        totaleDeceduti = csv.GetField<int>("deceduti"),
+                        totaleTamponi = csv.GetField<int>("tamponi"),
+                        totaleGuariti = csv.GetField<int>("dimessi_guariti"),
+                        totalePositivi = csv.GetField<int>("totale_positivi"),
+                        nuoviDeceduti = (csv.GetField<int>("deceduti") - regionep.totaleDeceduti),
+                        nuoviTamponi = (csv.GetField<int>("tamponi") - regionep.totaleTamponi)
+                    };
+
+                    records.Add(record);
+                }
+
+                return records;
+            }
+        }
+
+        public static List<Dati> Filtered(DateTime date)
+        {
+            List<Dati> filtered = new List<Dati>();
+
+            IAppCache cache = new CachingService();
+
+            Func<List<Dati>> getData = () => ReadCSV();
+
+            var records = cache.GetOrAdd("italia.Get", getData);
+
+            foreach (var record in records)
+            {
+                var filter = records[records.Count - 1];
+                filtered.Add(filter);
+
+                if (record.data.ToString("dd MMMM yyyy") == date.ToString("dd MMMM yyyy"))
+                {
+                    filter = record;
+                    filtered.Clear();
+                    filtered.Add(filter);
+                }
+            }
+
+            return filtered;
+        }
+
+        public static List<Dati> FilteredRegioni(DateTime date, string regione)
+        {
+            List<Dati> filtered = new List<Dati>();
+
+            IAppCache cache = new CachingService();
+
+            Func<List<Dati>> getData = () => ReadCSVRegioni();
+
+            var records = cache.GetOrAdd("regioni.Get", getData);
+
+            foreach (var record in records)
+            {
+                var filter = records[records.Count - 1];
+                filtered.Add(filter);
+
+                if (record.data.ToString("dd MMMM yyyy") == date.ToString("dd MMMM yyyy"))
+                {
+                    if (record.regione == regione)
+                    {
+                        filter = record;
+                        filtered.Clear();
+                        filtered.Add(filter);
+                    }
+                }
+            }
+
+            return filtered;
+        }
+
+        public class ChartSelector
+        {
+            public static ChartValues<int> nuoviPositivi(DateTime start, DateTime end)
+            {
+                ChartValues<int> filtered = new ChartValues<int>();
+                IAppCache cache = new CachingService();
+                Func<List<Dati>> getData = () => ReadCSV();
+                var records = cache.GetOrAdd("italia.Get", getData);
+
+                foreach (var record in records)
+                {
+                    var filter = records[records.Count - 1];
+
+                    if (DateTime.Compare(record.data, start) == 0 | DateTime.Compare(record.data, start) == 1)
+                    {
+                        if (DateTime.Compare(record.data, end) == 0 | DateTime.Compare(record.data, end) == -1)
+                        {
+                            filtered.Add(record.nuoviPositivi);
+                        }
+                    }
+                }
+                return filtered;
+            }
+
+            public static ChartValues<int> nuoviDeceduti(DateTime start, DateTime end)
+            {
+                ChartValues<int> filtered = new ChartValues<int>();
+                IAppCache cache = new CachingService();
+                Func<List<Dati>> getData = () => ReadCSV();
+                var records = cache.GetOrAdd("italia.Get", getData);
+
+                foreach (var record in records)
+                {
+                    var filter = records[records.Count - 1];
+
+                    if (DateTime.Compare(record.data, start) == 0 | DateTime.Compare(record.data, start) == 1)
+                    {
+                        if (DateTime.Compare(record.data, end) == 0 | DateTime.Compare(record.data, end) == -1)
+                        {
+                            filtered.Add(record.nuoviDeceduti);
+                        }
+                    }
+                }
+                return filtered;
+            }
+
+            public static ChartValues<int> totalePositivi(DateTime start, DateTime end)
+            {
+                ChartValues<int> filtered = new ChartValues<int>();
+                IAppCache cache = new CachingService();
+                Func<List<Dati>> getData = () => ReadCSV();
+                var records = cache.GetOrAdd("italia.Get", getData);
+
+                foreach (var record in records)
+                {
+                    var filter = records[records.Count - 1];
+
+                    if (DateTime.Compare(record.data, start) == 0 | DateTime.Compare(record.data, start) == 1)
+                    {
+                        if (DateTime.Compare(record.data, end) == 0 | DateTime.Compare(record.data, end) == -1)
+                        {
+                            filtered.Add(record.totalePositivi);
+                        }
+                    }
+                }
+                return filtered;
+            }
+
+            public static ChartValues<int> totaleDeceduti(DateTime start, DateTime end)
+            {
+                ChartValues<int> filtered = new ChartValues<int>();
+                IAppCache cache = new CachingService();
+                Func<List<Dati>> getData = () => ReadCSV();
+                var records = cache.GetOrAdd("italia.Get", getData);
+
+                foreach (var record in records)
+                {
+                    var filter = records[records.Count - 1];
+
+                    if (DateTime.Compare(record.data, start) == 0 | DateTime.Compare(record.data, start) == 1)
+                    {
+                        if (DateTime.Compare(record.data, end) == 0 | DateTime.Compare(record.data, end) == -1)
+                        {
+                            filtered.Add(record.totaleDeceduti);
+                        }
+                    }
+                }
+                return filtered;
+            }
+
+            public static ChartValues<int> nuoviTamponi(DateTime start, DateTime end)
+            {
+                ChartValues<int> filtered = new ChartValues<int>();
+                IAppCache cache = new CachingService();
+                Func<List<Dati>> getData = () => ReadCSV();
+                var records = cache.GetOrAdd("italia.Get", getData);
+
+                foreach (var record in records)
+                {
+                    var filter = records[records.Count - 1];
+
+                    if (DateTime.Compare(record.data, start) == 0 | DateTime.Compare(record.data, start) == 1)
+                    {
+                        if (DateTime.Compare(record.data, end) == 0 | DateTime.Compare(record.data, end) == -1)
+                        {
+                            filtered.Add(record.nuoviTamponi);
+                        }
+                    }
+                }
+                return filtered;
+            }
+
+            public static ChartValues<int> totaleTamponi(DateTime start, DateTime end)
+            {
+                ChartValues<int> filtered = new ChartValues<int>();
+                IAppCache cache = new CachingService();
+                Func<List<Dati>> getData = () => ReadCSV();
+                var records = cache.GetOrAdd("italia.Get", getData);
+
+                foreach (var record in records)
+                {
+                    var filter = records[records.Count - 1];
+
+                    if (DateTime.Compare(record.data, start) == 0 | DateTime.Compare(record.data, start) == 1)
+                    {
+                        if (DateTime.Compare(record.data, end) == 0 | DateTime.Compare(record.data, end) == -1)
+                        {
+                            filtered.Add(record.totaleTamponi);
+                        }
+                    }
+                }
+                return filtered;
+            }
+
+            public static ChartValues<int> totaleGuariti(DateTime start, DateTime end)
+            {
+                ChartValues<int> filtered = new ChartValues<int>();
+                IAppCache cache = new CachingService();
+                Func<List<Dati>> getData = () => ReadCSV();
+                var records = cache.GetOrAdd("italia.Get", getData);
+
+                foreach (var record in records)
+                {
+                    var filter = records[records.Count - 1];
+
+                    if (DateTime.Compare(record.data, start) == 0 | DateTime.Compare(record.data, start) == 1)
+                    {
+                        if (DateTime.Compare(record.data, end) == 0 | DateTime.Compare(record.data, end) == -1)
+                        {
+                            filtered.Add(record.totaleGuariti);
+                        }
+                    }
+                }
+                return filtered;
+            }
+
+            public static ChartValues<int> totaleCasi(DateTime start, DateTime end)
+            {
+                ChartValues<int> filtered = new ChartValues<int>();
+                IAppCache cache = new CachingService();
+                Func<List<Dati>> getData = () => ReadCSV();
+                var records = cache.GetOrAdd("italia.Get", getData);
+
+                foreach (var record in records)
+                {
+                    var filter = records[records.Count - 1];
+
+                    if (DateTime.Compare(record.data, start) == 0 | DateTime.Compare(record.data, start) == 1)
+                    {
+                        if (DateTime.Compare(record.data, end) == 0 | DateTime.Compare(record.data, end) == -1)
+                        {
+                            filtered.Add(record.totaleCasi);
+                        }
+                    }
+                }
+                return filtered;
+            }
+
+            public static ChartValues<string> data(DateTime start, DateTime end)
+            {
+                ChartValues<string> filtered = new ChartValues<string>();
+                IAppCache cache = new CachingService();
+                Func<List<Dati>> getData = () => ReadCSV();
+                var records = cache.GetOrAdd("italia.Get", getData);
+
+                foreach (var record in records)
+                {
+                    var filter = records[records.Count - 1];
+
+                    if (DateTime.Compare(record.data, start) == 0 | DateTime.Compare(record.data, start) == 1)
+                    {
+                        if (DateTime.Compare(record.data, end) == 0 | DateTime.Compare(record.data, end) == -1)
+                        {
+                            filtered.Add(record.data.ToString("dd MMM"));
+                        }
+                    }
+                }
+                return filtered;
+            }
+        }
+    }
+}
